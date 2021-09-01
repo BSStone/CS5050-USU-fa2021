3a1212ffa263e8b02de99fa2c7f14b77673533e5
Brandon Stone <bstone.cs@gmail.com>
2021-09-01 13:27:40 -0600

    Started work on the game. Created user input and added the algorithm. Checking with teacher to see if this is correct approach

--- Nim/main.py
+++ Nim/main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+def nim(n):
+    computer_turn = False
+    while True:     # Game loop, only end when game ends.
+        if computer_turn:
+            turn(n)
+            computer_turn = False
+            while True:     # Test user input
+                user = int(input("How many stones would you like to take away? <1> or <2>: "))
+                if user < 3 and user > 0:
+                    break
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+        turn(n)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def win(n):
+    print(n)
+    if n == 0:
+        return True
+    if n == 1:
+        return False
+    return not (win(n-1) and win(n-2))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def turn(n):
+    if n == 1:
+        return 1
+    if n == 2:
+         return 1
+    if win(n - 1):
+        return 1
+    if win(n - 2):
+        return 2
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
