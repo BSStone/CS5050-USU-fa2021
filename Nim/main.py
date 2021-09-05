72561a734fc23e461bdcaa530e2e7d562ca4e8fa
Brandon Stone <bstone.cs@gmail.com>
2021-09-05 14:43:17 -0600

    It's now working. Still need memoized version and graph.

--- Nim/main.py
+++ Nim/main.py
@@ -1,33 +1,49 @@
 def nim(n):
     computer_turn = False
-    while True:     # Game loop, only end when game ends.
+    while True:  # Game loop, only end when game ends.
         if computer_turn:
-            turn(n)
+            algo_output = computerTurn(n)
+            print("The algorithm takes " + str(algo_output) + " stones.")
+            n = n - algo_output
+            print("There are " + str(n) + " stones left.")
             computer_turn = False
-            while True:     # Test user input
+        else:
+            if n == 0:
+                print("Congrats, you beat the algorithm!")
+                exit(1)
+            while True:  # Test user input
                 user = int(input("How many stones would you like to take away? <1> or <2>: "))
                 if user < 3 and user > 0:
                     break
-
-        turn(n)
+            computer_turn = True
+            n = n - user
+            if n < 0:
+                print("You lose!")
+                exit(2)
+            print("You take " + str(user) + " stones.")
+            print("There are " + str(n) + " stones left.")
+            if n == 0:
+                print("You lose!")
+                exit(1)
 
 
 def win(n):
-    print(n)
     if n == 0:
         return True
     if n == 1:
         return False
-    return not (win(n-1) and win(n-2))
+    return not (win(n - 1) and win(n - 2))
 
 
-def turn(n):
+def computerTurn(n):
     if n == 1:
         return 1
     if n == 2:
-         return 1
-    if win(n - 1):
         return 1
     if win(n - 2):
+        return 1
+    if win(n - 1):
         return 2
 
+
+nim(10)
